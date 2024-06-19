9e01955595a686f1063acf9617c4f10022a3463c
jyip6 <ye1846955797@gmail.com>
2024-06-18 23:54:16 -0700

    a3 JavaScript

---
+++ src/Kanbas/Database/index.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+import courses from "./courses.json";
+import modules from "./modules.json";
+import assignments from "./assignments.json";
+import users from "./users.json";
+import enrollments from "./enrollments.json";
+import grades from "./grades.json";
+export {  courses, modules, assignments, users, enrollments, grades   };
